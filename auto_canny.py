34323e60d2b7b76b434fd93d61dc03bac3f1725c
Patrick Ellis <patrick.j.ellis@gmail.com>
2020-02-21 11:06:40 -0500

    add canny script

---
+++ auto_canny.py
@@ -0,0 +1,45 @@
+# adapted from https://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+
+import ntpath
+import os
+import argparse
+import cv2
+import numpy as np
+
+
+def auto_canny(image: str, sigma: float = 0.33) -> np.ndarray:
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    median = np.median(blurred)
+    lower = int(max(0, (1.0 - sigma) * median))
+    upper = int(min(255, (1.0 + sigma) * median))
+    edged = cv2.Canny(image, lower, upper)
+    return edged
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "images",
+        metavar="image",
+        type=argparse.FileType("r"),
+        nargs="+",
+        help="image filenames to process, as positional args"
+    )
+    arg_parser.add_argument(
+        "-o",
+        "--out-dir",
+        required=True,
+        help="output, directory. where to write the edge detection images"
+    )
+    args = vars(arg_parser.parse_args())
+
+    for image_path in [i.name for i in args["images"]]:
+        auto = auto_canny(image=cv2.imread(image_path))
+        filename = ntpath.split(image_path)[-1]
+        cv2.imwrite(os.path.join(args["out_dir"], filename), auto)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
