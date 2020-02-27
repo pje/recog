9169dff17aa3888c76838d23ba0100a68c78946a
Patrick Ellis <patrick.j.ellis@gmail.com>
2020-02-27 12:37:26 -0500

    delete some comments

--- pix2pix2.py
+++ pix2pix2.py
@@ -161,23 +161,23 @@ def upsample(filters, size, apply_dropout=False):
 def Generator():
     inputs = tf.keras.layers.Input(shape=[256, 256, 3])
     down_stack = [
-        downsample(64, 4, apply_batchnorm=False),  # (bs, 128, 128, 64)
-        downsample(128, 4),  # (bs, 64, 64, 128)
-        downsample(256, 4),  # (bs, 32, 32, 256)
-        downsample(512, 4),  # (bs, 16, 16, 512)
-        downsample(512, 4),  # (bs, 8, 8, 512)
-        downsample(512, 4),  # (bs, 4, 4, 512)
-        downsample(512, 4),  # (bs, 2, 2, 512)
-        downsample(512, 4),  # (bs, 1, 1, 512)
+        downsample(64, 4, apply_batchnorm=False),
+        downsample(128, 4),
+        downsample(256, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
     ]
     up_stack = [
-        upsample(512, 4, apply_dropout=True),  # (bs, 2, 2, 1024)
-        upsample(512, 4, apply_dropout=True),  # (bs, 4, 4, 1024)
-        upsample(512, 4, apply_dropout=True),  # (bs, 8, 8, 1024)
-        upsample(512, 4),  # (bs, 16, 16, 1024)
-        upsample(256, 4),  # (bs, 32, 32, 512)
-        upsample(128, 4),  # (bs, 64, 64, 256)
-        upsample(64, 4),  # (bs, 128, 128, 128)
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4),
+        upsample(256, 4),
+        upsample(128, 4),
+        upsample(64, 4),
     ]
     initializer = tf.random_normal_initializer(0., 0.02)
     last = tf.keras.layers.Conv2DTranspose(
@@ -186,7 +186,7 @@ def Generator():
         padding='same',
         kernel_initializer=initializer,
         activation='tanh'
-    )  # (bs, 256, 256, 3)
+    )
     x = inputs
     # Downsampling through the model
     skips = []
@@ -223,27 +223,27 @@ def Discriminator():
     initializer = tf.random_normal_initializer(0., 0.02)
     inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
     tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
-    x = tf.keras.layers.concatenate([inp, tar])  # (bs, 256, 256, channels*2)
-    down1 = downsample(64, 4, False)(x)  # (bs, 128, 128, 64)
-    down2 = downsample(128, 4)(down1)  # (bs, 64, 64, 128)
-    down3 = downsample(256, 4)(down2)  # (bs, 32, 32, 256)
-    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
+    x = tf.keras.layers.concatenate([inp, tar])
+    down1 = downsample(64, 4, False)(x)
+    down2 = downsample(128, 4)(down1)
+    down3 = downsample(256, 4)(down2)
+    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)
     conv = tf.keras.layers.Conv2D(
         512,
         4,
         strides=1,
         kernel_initializer=initializer,
         use_bias=False
-    )(zero_pad1)  # (bs, 31, 31, 512)
+    )(zero_pad1)
     batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
     leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
-    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
+    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)
     last = tf.keras.layers.Conv2D(
         1,
         4,
         strides=1,
         kernel_initializer=initializer
-    )(zero_pad2)  # (bs, 30, 30, 1)
+    )(zero_pad2)
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 
